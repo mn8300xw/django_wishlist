b8731f5a651423d31bf10486357f3f4d0a496feb
Vanessa M. <mn8300xw@go.minnstate.edu>
2026-03-25 12:46:25 -0500

    List non-visited places in alphabetic order.

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Place
 
 # Create your views here.
 
 def place_list(request):
-    return render(request, 'travel_wishlist/wishlist.html')
+    places = Place.objects.filter(visited=False).order_by('name')
+    return render(request, 'travel_wishlist/wishlist.html', {'places': places})
