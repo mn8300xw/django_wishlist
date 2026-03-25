e7b5c77f08e014dcdb225c4a564c648a603146a2
Vanessa M. <mn8300xw@go.minnstate.edu>
2026-03-25 14:38:32 -0500

    added about and visited pages

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Place
 from .forms import NewPlaceForm
 
 # Create your views here.
 
 def place_list(request):
+
+    if request.method == 'POST':
+        # create new place
+        form = NewPlaceForm(request.POST) # creating a form from data in the request
+        place = form.save() # creating a model object from form
+        if form.is_valid(): # validation against DB constraints
+            place.save() # saves place to db
+            return redirect('place_list') # reloads home page
+
     places = Place.objects.filter(visited=False).order_by('name')
-    new_place_form = NewPlaceForm()
+    new_place_form = NewPlaceForm() # used to create HTML
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
+
+def places_visited(request):
+    visited = Place.objects.filter(visited=True).order_by('name')
+    return render(request, 'travel_wishlist/visited.html', {'visited': visited})
+
+def about(request):
+    author = 'Vanessa'
+    about = 'A website to create a list of places to visit'
+    return render(request, 'travel_wishlist/about.html', {'author': author, 'about': about})
+
