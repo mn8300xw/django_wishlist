8daaf4a5b81e7732212e0193a38cf2e8d1d9aff6
Vanessa M. <mn8300xw@go.minnstate.edu>
2026-03-25 12:51:40 -0500

    added form

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Place
+from .forms import NewPlaceForm
 
 # Create your views here.
 
 def place_list(request):
     places = Place.objects.filter(visited=False).order_by('name')
-    return render(request, 'travel_wishlist/wishlist.html', {'places': places})
+    new_place_form = NewPlaceForm()
+    return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
